20869f9c38aedc60bfcfe99e775f4eaafcad0848
Lucas Jaime <lucasgabrieljaime99@gmail.com>
2023-10-24 03:55:22 -0300

    agregamos el sexto ejercicio

---
+++ js/ejercicio-6.js
@@ -0,0 +1,7 @@
+let primerNumero = parseInt(prompt("Ingrese el primer numero"))
+let segundoNumero = parseInt(prompt("Ingrese el segundo numero"))
+if(primerNumero>segundoNumero){
+    console.log(primerNumero+" es el numero mas grande")
+}else{
+    console.log(segundoNumero+" es el numero mas grande")
+}
