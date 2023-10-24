3227d54e3ea733451cebedb5deee4a22682febca
Lucas Jaime <lucasgabrieljaime99@gmail.com>
2023-10-24 03:38:10 -0300

    agregamos el quinto ejercicio

---
+++ js/ejercicio-5.js
@@ -0,0 +1,3 @@
+let primerNumero = parseInt(prompt("Ingrese el primer numero"))
+let segundoNumero = parseInt(prompt("Ingrese el segundo numero"))
+console.log("El resultado de sumar el primer y segundo numero es: "+(primerNumero+segundoNumero))
