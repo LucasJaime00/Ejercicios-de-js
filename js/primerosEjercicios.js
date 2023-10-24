93af79bc2c3390fc77c4d382905b4df297c2321a
Lucas Jaime <lucasgabrieljaime99@gmail.com>
2023-10-24 04:58:45 -0300

    agregamos los ejercicios

---
+++ js/primerosEjercicios.js
@@ -0,0 +1,15 @@
+let numero1 = 10
+let numero2 = 15
+console.log(("el resultado de numero1 + numero 2 es: ")+(numero1+numero2))
+
+let numero3 = 18
+let numero4 = 15
+console.log(("el resultado de numero3 - numero4 es: ")+(numero3-numero4))
+
+let numero5 = 5
+let numero6 = 4
+console.log(("el resultado de numero5 * numero6 es: ")+(numero5*numero6))
+
+let numero7 = 16
+let numero8 = 4
+console.log(("el resultado de numero7 / numero8 es: ")+(numero7/numero8))
